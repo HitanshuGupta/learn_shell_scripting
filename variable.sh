17720bf161e294ac5c1e2b7737dca7ea3e402dc4
Hitanshu <hitanshugupta212@gmail.com>
2023-07-31 21:21:07 +0530

    variable script

---
+++ variable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Script to show how to use varibales
+
+a=10
+name="Hitanshu"
+age=21
+
+echo "My name is $name and age is $age"
+
+name="bittu"
+
+echo "My name is  $name"
+
+# Var to store the output of a commond
+
+HostName=$(hostname)
+echo "Name of this machine is $HostName"
+
